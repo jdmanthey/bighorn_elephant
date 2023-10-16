52f31815ead34be7aa9f64b83c6116a1218d5830
Joe Manthey <jdmanthey@gmail.com>
2023-10-16 12:05:15 -0500

    Update 01_filter_process_structure.sh

--- 01_filter_process_structure.sh
+++ 01_filter_process_structure.sh
@@ -18,6 +18,7 @@ mkdir 07_stairway_all
 cp populations.snps.vcf .
 
 # copy the snps files for the different time periods for elephant mountain
+# these were run in separate STACKS analyses
 cp populationsEM20*snps.vcf .
 cd 04_stairway_2000
 mv ../populationsEM2000.snps.vcf .
@@ -44,8 +45,11 @@ bcftools reheader -s sample_rename.txt -o elephant.vcf populations.snps.vcf
 ########################################
 
 # run vcftools to filter for the 3 non-stairway datasets
-# filter for admixture and pca
+# filter for pca
 vcftools --vcf ${workdir}/elephant.vcf --keep keep_structure.txt --max-missing 0.9 --minGQ 20 --minDP 8 --max-meanDP 100 --min-alleles 2 --max-alleles 2 --mac 3 --thin 10000 --max-maf 0.49 --recode --recode-INFO-all --out ${workdir}/01_structure/structure
+
+# filter for admixture (only subset of elephant mountain individuals)
+vcftools --vcf ${workdir}/elephant.vcf --keep keep_admixture.txt --max-missing 0.9 --minGQ 20 --minDP 8 --max-meanDP 100 --min-alleles 2 --max-alleles 2 --mac 3 --thin 10000 --max-maf 0.49 --recode --recode-INFO-all --out ${workdir}/01_structure/admixture
 
 # filter for phylo, diversity
 vcftools --vcf ${workdir}/elephant.vcf --keep keep_phylo.txt --max-missing 0.9 --minGQ 20 --minDP 8 --max-meanDP 100 --min-alleles 2 --max-alleles 2 --max-maf 0.49 --recode --recode-INFO-all --out ${workdir}/02_phylo/phylo
@@ -131,22 +135,27 @@ cd 01_structure/
 # make chromosome map for the vcf
 grep -v "#" structure.recode.vcf | cut -f 1 | uniq | awk '{print $0"\t"$0}' > chrom_map.txt
 
-# run vcftools for the vcf intended for admixture and PCA with plink output
+# run vcftools for the vcf intended for PCA with plink output
 vcftools --vcf structure.recode.vcf --plink --chrom-map chrom_map.txt --out structure
 
-# convert with plink for each of the datasets that we will estimate admixture and pca
+# convert with plink for pca dataset
 plink --file structure --recode12 --allow-extra-chr --out structure_plink
 
-# run each dataset for pca
-# all bighorn
+# run  dataset for pca
 plink --file structure_plink --pca --allow-extra-chr --out structure_plink_pca
+
+# run vcftools for the vcf intended for admixture with plink output
+vcftools --vcf structure.recode.vcf --plink --chrom-map chrom_map.txt --out admixture
+
+# convert with plink for admixture dataset
+plink --file admixture --recode12 --allow-extra-chr --out admixture_plink
+
 
 # run each dataset for admixture
 # all bighorn 
-for K in 1 2 3 4 5 6 7; do admixture --cv structure_plink.ped $K  | tee log_elephant_${K}.out; done
+for K in 1 2 3 4 5 6 7; do admixture --cv admixture_plink.ped $K  | tee log_elephant_${K}.out; done
 
 cd ..
-
 
 
 
